e935eb4f6e54cbdf8747c75f160372d46930473e
SudipDahal12 <dahal3164@gmail.com>
2025-10-20 21:20:58 +0545

    new file firebase-config added

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+// This file should be added to .gitignore
+
+export const firebaseConfig = {
+        apiKey: "AIzaSyBIhh_bVeC3x02Yo2pji9ZcT-TbbSAXzJ0",
+        authDomain: "notification-webapp-205cf.firebaseapp.com",
+        projectId: "notification-webapp-205cf",
+        storageBucket: "notification-webapp-205cf.firebasestorage.app",
+        messagingSenderId: "224899952309",
+        appId: "1:224899952309:web:3b07c841e1b4527f9c98fc"
+        };
